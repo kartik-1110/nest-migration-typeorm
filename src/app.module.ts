fa3b66aeeabefee6c045a2c1d232a09b0a996011
Kartik Pal <kartik.pal@konverge.ai>
2023-02-23 12:55:52 +0530

    feat: added new endpoint picture

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { dataSourceOptions } from 'db/data-source';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PictureModule } from './picture/picture.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     UsersModule,
+    PictureModule,
   ],
   controllers: [AppController],
   providers: [AppService],
